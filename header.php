b2b1faae2dceb857bde15b1769ae80b1f99b10c5
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-20 16:24:15 +0100

    change nav banner footer functions

--- header.php
+++ header.php
@@ -6,21 +6,9 @@
   <meta name="author" content="">
 
   <title>Freelancer - Start Bootstrap Theme</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="http://localhost:8888/wp-dev/wp-content/themes/wp-coding/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-  <!-- Plugin CSS -->
-  <link href="http://localhost:8888/wp-dev/wp-content/themes/wp-coding/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-
-  <!-- Custom styles for this template -->
-  <link href="http://localhost:8888/wp-dev/wp-content/themes/wp-coding/css/freelancer.min.css" rel="stylesheet">
-
+<?php
+  wp_head();
+?>
 </head>
 
 <body id="page-top">
@@ -28,7 +16,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><?php echo get_bloginfo('name'); ?></a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -45,6 +33,14 @@
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
           </li>
         </ul>
+        <?php
+        wp_nav_menu([
+            // 'menu' => 'main-menu',
+          'menu_class' => 'navbar-nav ml-auto',
+          'theme-location' => 'main-menu',
+          'container' => ''
+        ]);
+        ?>
       </div>
     </div>
   </nav>
