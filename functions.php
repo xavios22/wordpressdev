0f0bdfa9f8c32a984354385c5dc933cecb0056de
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-20 16:58:04 +0100

    ajout d'une section pour l'affichage des images du post

--- functions.php
+++ functions.php
@@ -36,6 +36,35 @@ function register_main_menu()
   register_nav_menu('main-menu', 'Menu principal dans le header.');
 }
  add_action('after_setup_theme', 'register_main_menu'); 
-  
+  /** 
+ *Fonction qui ajoute des attributes au balise a des nav_menu
+ *
+ * @param [type] $att
+ * @param [type] $item
+ * @param [type] $args
+ * @return void
+ */
+function ajout_menu_a_class($atts, $item, $args)
+{
+  $class = 'nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger'; // or something based on $item
+  $atts['class'] = $class;
+  return $atts;
+}
+ // Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
+// les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
+add_filter('nav_menu_link_attributes', 'ajout_menu_a_class', 10, 3); 
+/**
+ * Ajout la fonctionnalité d'un ajout d'image pour les posts pour ce thème ci
+ *
+ * @return void
+ */
+function ajout_image_article()
+{
+  //Ajout de la fonctionnalité d'ajout d'image pour les posts pour ce thème ci
+  add_theme_support('post-thumbnails');
+  $test = 0;
+}
+ // Ajout d'un écouteur d'événement pour activer les images mise en avant pour les post (article)
+add_action('init', 'ajout_image_article');
 
  ?>
