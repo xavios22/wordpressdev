1225e6c5774cff046cda2be8e949805f3cfb3db6
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-22 09:48:35 +0100

    creation de fichier enqueue pour la meillieur struturation de notre code et plus lisible

--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
+//Pour ne pas avoir de fichier contenant trop de ligne de code nous allons repartir le tout dans des fichiers spécifiques afin de rendre le tout plus lisible.
+// Nous découvrons ici la function require_once() de php qui nous permet d'importer des fichiers
+// Nous découvrons également la fonction get_template_directory() qui renvoi le chemin du dossier du thème actif sur (à ne pas confondre avec get_template_directory_uri() qui renvoi une url) 
+
+
+require_once(get_template_directory() . '/includes/enqueue-script.php');
+
+
  /**
  * Fonction qui va ajouter des scripts dynamiquement afin que l'on puisse les inclures dans le thème avec wp_head() et wp_footer()
  *
  * @return void
  */
+
 function ajout_css_js()
 {
   // Ajout des scripts css
@@ -66,5 +75,65 @@ function ajout_image_article()
 }
  // Ajout d'un écouteur d'événement pour activer les images mise en avant pour les post (article)
 add_action('init', 'ajout_image_article');
+/**
+ * Fonction qui ajoute la possibilité de customiser la partie personnalisation du thème
+ * //https://developer.wordpress.org/themes/customize-api/customizer-objects/
+ *
+ * @param [type] $wp_customize
+ * @return void
+ */
+function ajout_personnalisation_about($wp_customize)
+{
+  // Ajout d'un panel avec des options
+  // Attention, un panel ne s'affichera que s'il contient des sections
+  //https://developer.wordpress.org/reference/classes/wp_customize_manager/add_panel/
+  $wp_customize->add_panel('coding-panel-about', [
+    'title' => __('Section About'),
+    'Description' => __('Personnalisation de la section about')
+  ]);
+   // Ajout d'une section à un panel définie, si pas de panel défini, alors la section sera visible directement
+  // Attention il faut que la panel ait déjà été ajouter pour que la section puisse s'y greffer.
+  // Attention une section ne s'affichera que si elle contient des controls.
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_section/
+  $wp_customize->add_section('coding-about-section-text', [
+    'panel' => 'coding-panel-about',
+    'title' => __('Personnalisation du texte'),
+    'description' => __('Personnalisez le texte')
+  ]);
+  
+  // Ajout d'un setting qui contiendra des informations dans la base de donnée sous la clé correspondant à son id (premier paramètre)
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_setting/
+  $wp_customize->add_setting('coding-about-text-left', [
+    'type' => 'theme_mod',
+    'sanitize_callback' => 'sanitize_textarea_field'
+  ]);
+    // Ajout d'un setting qui contiendra des informations dans la base de donnée sous la clé correspondant à son id (premier paramètre)
+   // La clé est utilisé pour récuperer les valeurs dans le thème grâce à la fonction get_theme_mod()
+   // Attention la ligne précédente n'est valable que si le 'type' du setting est défini à 'theme_mod'
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_setting/
+  $wp_customize->add_setting('coding-about-text-right', [
+    'type' => 'theme_mod',
+    'sanitize_callback' => 'sanitize_textarea_field'
+  ]);
+   // Ajout d'un control (un label avec input et autre information). Le control doit être attaché à une section ainsi qu'à un setting.
+  // Attention le setting doit déjà être créer afin que le control puisse s'ajouter.
+  // Attention un control ne s'affiche que s'il est lié à un setting.
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_control/
+  $wp_customize->add_control('coding-about-text-left-control', [
+    'section' => 'coding-about-section-text',
+    'settings' => 'coding-about-text-left',
+    'label' => __('Texte colonne gauche'),
+    'description' => __('Personnalisez le texte de la colonne gauche'),
+    'type' => 'textarea'
+  ]);
+  $wp_customize->add_control('coding-about-text-right-control', [
+    'section' => 'coding-about-section-text',
+    'settings' => 'coding-about-text-right',
+    'label' => __('Texte colonne right'),
+    'description' => __('Personnalisez le texte de la colonne droite'),
+    'type' => 'textarea'
+  ]);
+}
+add_action('customize_register', 'ajout_personnalisation_about');
 
  ?>
--- templates/about.php
+++ templates/about.php
@@ -1,21 +1,27 @@
+<?php
+// https://developer.wordpress.org/reference/functions/get_theme_mod/
+// Attention dans ce template on utilise un raccourci pour <?php echo par <?= ceci n'est possible que si la config php.ini le permet short_open_tag. À ne faire que si vous avez le control de la config php.ini
+$text_column_left = get_theme_mod('coding-about-text-left', __('Texte about gauche'));
+$text_column_right = get_theme_mod('coding-about-text-right', __('Texte about Droit'));
+?>
 <!-- About Section -->
 <section class="bg-primary text-white mb-0" id="about">
-    <div class="container">
-      <h2 class="text-center text-uppercase text-white">About</h2>
-      <hr class="star-light mb-5">
-      <div class="row">
-        <div class="col-lg-4 ml-auto">
-          <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
-        </div>
-        <div class="col-lg-4 mr-auto">
-          <p class="lead">Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
-        </div>
+  <div class="container">
+    <h2 class="text-center text-uppercase text-white">About</h2>
+    <hr class="star-light mb-5">
+    <div class="row">
+      <div class="col-lg-4 ml-auto">
+        <p class="lead"><?= $text_column_left; ?></p>
       </div>
-      <div class="text-center mt-4">
-        <a class="btn btn-xl btn-outline-light" href="#">
-          <i class="fas fa-download mr-2"></i>
-          Download Now!
-        </a>
+      <div class="col-lg-4 mr-auto">
+        <p class="lead"><?= $text_column_right; ?></p>
       </div>
     </div>
-  </section>
+    <div class="text-center mt-4">
+      <a class="btn btn-xl btn-outline-light" href="#">
+        <i class="fas fa-download mr-2"></i>
+        Download Now!
+      </a>
+    </div>
+  </div>
+</section>
