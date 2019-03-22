73dbb6e65cb3627b95d1127cf13404a000162cad
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-22 14:02:44 +0100

    site dynamique et modification de template

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,70 +1,33 @@
-
-  <!-- Portfolio Grid Section -->
-  <section class="portfolio" id="portfolio">
+<?php
+$args = [
+  'post_type' => 'post',
+  'posts_per_page' => 6,
+  'category_name' => 'portfolio'
+];
+$query = new WP_Query($args);
+?>
+<!-- Portfolio Grid Section -->
+<section class="portfolio" id="portfolio">
     <div class="container">
-      <h2 class="text-center text-uppercase text-secondary mb-0">Portfolio</h2>
-      <hr class="star-dark mb-5">
-      <div class="row">
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
+        <h2 class="text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+        <hr class="star-dark mb-5">
+        <div class="row">
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+            <div class="col-md-6 col-lg-4">
+                <a class="portfolio-item d-block mx-auto" href="<?php the_permalink(); ?>">
+                    <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                        <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                            <i class="fas fa-search-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="<?php the_post_thumbnail_url(); ?>" alt="">
+                </a>
             </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/cabin.png" alt="">
-          </a>
+            <?php 
+          endwhile;
+          // Cette fonction réactualise les $post data en fonction du context du wp hierarchie
+          wp_reset_postdata();
+          ?>
         </div>
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-2">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/cake.png" alt="">
-          </a>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-3">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/circus.png" alt="">
-          </a>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-4">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/game.png" alt="">
-          </a>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-5">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/safe.png" alt="">
-          </a>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-6">
-            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
-              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                <i class="fas fa-search-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="http://localhost:8888/wp-dev/wp-content/themes/theme/img/portfolio/submarine.png" alt="">
-          </a>
-        </div>
-      </div>
     </div>
-  </section>
+</section> 
