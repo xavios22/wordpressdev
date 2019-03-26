814fda000a5a58dc0dd07235c816e8697460de6d
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-26 10:05:52 +0100

    ajout d'un formularire de recherche dans le header ainsi qu'un fichier search.php

--- header.php
+++ header.php
@@ -29,6 +29,12 @@
                 <i class="fas fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
+                <!-- On ajoute ici un formulaire de recherche qui va chercher tout les articles qui contienent un certain mot. Le formulaire doit avoir la méthode get -->
+                <form class="form-inline" action="<?php echo get_site_url(); ?>" method="get">
+                    <!--le name de cette input est "s" pour string afin de rechercher un mot-->
+                    <input class="form-control" type="text" name="s" id="">
+                    <button class="btn bg-white ml-3" type="submit"><i class="fa fa-search"></i></button>
+                </form>
                 <?php
         // https://developer.wordpress.org/reference/functions/wp_nav_menu/
                 wp_nav_menu([
