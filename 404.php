e82559e7b608095a2d65fd992ab92bd96ec3d871
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-26 11:00:33 +0100

    ajout d'un fichier 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+ get_header();
+// Ce fichier template spécial de wordpress est appelé pour afficher une page 404 lorque l'url ne correspond à rien
+// https://wphierarchy.com/
+?>
+
+ <div class="container single-post-container">
+    <div class="jumbotron">
+        <h1 class="text-center">OUPS la page que vous cherchez n'existe pas</h1>
+        <h1 class="text-center">404</h1>
+    </div>
+</div>
+<?php
+ get_footer();
+ ?>  
