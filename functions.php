09b8d037838c21738d2298a4bf5c1585f3b53f8a
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-26 11:14:21 +0100

    on va tout mettre dans un dossier plugins qui va se charger de contenir toutes les plugins

--- functions.php
+++ functions.php
@@ -9,34 +9,5 @@ require_once(INCLUDE_DIR . '/menu.php');
 require_once(INCLUDE_DIR . '/theme-setup.php');
 require_once(INCLUDE_DIR . '/customizer.php');
 require_once(INCLUDE_DIR . '/sidebars.php');
+require_once('plugins/plugins.php');
 //Ajout d'une section pour lecture
-function ajout_section_lecture()
-{
-  // Enregistrement d'une option
-  // le premier argument est le nom de la page qui va contenir cette option
-  // le second argument est le nom de l'option qui sera également le name de l'input
-  register_setting('reading', 'nombre_portfolio_home');
-  // Ajout d'une section dans une page
-  /**
-   * premier argument est l'id (slug)
-   * deuxième argument est le titre de la section
-   * troisième argument est le nom de la fonction callback
-   * Quatrième argument est le slug de la page qui affichera cette section
-   */
-  add_settings_section('home_portfolio', __('nombre de portfolio affiché'), 'section_portfolio', 'reading');
-}
-/**
- * Fonction callback qui affichera la nouvelle section
- *
- * @return void
- */
-function section_portfolio()
-{
-  // on récupère la valeur de l'option
-  $nombre_portfolio = (get_option('nombre_portfolio_home')) ? get_option('nombre_portfolio_home') : '';
-  // on affiche l'input
-  ?>
-<input type="number" value="<?= $nombre_portfolio; ?>" name="nombre_portfolio_home">
-<?php
-}
-add_action('admin_init', 'ajout_section_lecture');
