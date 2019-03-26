d24d771115e0d0d50d2966bf9999c30bab990e71
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-26 09:48:59 +0100

    ajout d'un emplacement colonne droit

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -4,8 +4,16 @@ class MgWidgetRegister
   public static function register()
   {
     register_sidebar(array(
-      'name'          => __('Footer 1ere colonne'),
+      'name'          => __('Footer colonne gauche'),
       'id'            => 'footer-left',
+      'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h4 class="text-uppercase mb-4">',
+      'after_title'   => '</h4>',
+    ));
+    register_sidebar(array(
+      'name'          => __('Footer colonne droite'),
+      'id'            => 'footer-right',
       'before_widget' => '<div>',
       'after_widget'  => '</div>',
       'before_title'  => '<h4 class="text-uppercase mb-4">',
