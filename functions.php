3676eb239afefa2ab41b0b6828062017ebbc69ad
Xavy Mora <xavymora@MacBook-Air-de-Xavy.local>
2019-03-26 09:36:20 +0100

    ajout d'un emplacement pour affiche les widgets

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ require_once(INCLUDE_DIR . '/enqueue-script.php');
 require_once(INCLUDE_DIR . '/menu.php');
 require_once(INCLUDE_DIR . '/theme-setup.php');
 require_once(INCLUDE_DIR . '/customizer.php');
+require_once(INCLUDE_DIR . '/sidebars.php');
 
 
  
